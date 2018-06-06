247fc3b946b7dc2115e75c3ca3d33589e8aea4af
Adrian Bala <aadrianbala@gmail.com>
2018-06-06 15:49:38 +0200

    Add Timer class

---
+++ src/main/java/Timer.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.TimeUnit;
+
+public class Timer implements Runnable {
+
+    private int second;
+    private String name;
+    private static int timerCount = 0;
+    private final int threadId = ++timerCount;
+
+    public Timer(String name) {
+        this.name = name;
+        second = 0;
+    }
+
+    public Timer(String name, int second) {
+        this.name = name;
+        this.second = second;
+    }
+
+    public void run() {
+        try {
+            while(true) {
+                TimeUnit.SECONDS.sleep(1);
+                second++;
+            }
+        } catch(InterruptedException e) {
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + " Thread id: " + threadId + " Second: " + second;
+    }
+}
