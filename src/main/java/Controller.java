45d9682080e73054284a1c4fe12e066f7f4ce73b
Adrian Bala <aadrianbala@gmail.com>
2018-06-07 13:47:22 +0200

    Implement controller class

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -3,30 +3,65 @@ import java.util.Scanner;
 
 public class Controller {
 
-    public static void start() {
-        View view = new View();
+    ThreadsContainer container;
+    View view;
+
+    public Controller() {
+        container = new ThreadsContainer();
+        view = new View();
+    }
+
+    public void start() {
+        int commandIndex = 0;
         String [] input;
         boolean isExit = false;
         while(!isExit) {
             input = view.getUserInput();
-            switch(input[0].toLowerCase()) {
+            switch(input[commandIndex].toLowerCase()) {
                 case "start":
-                    System.out.println("start");
+                    createThread(input);
                     break;
                 case "check":
-                    System.out.println("check");
+                    check(input);
                     break;
                 case "stop":
-                    System.out.println("stop");
+                    stopTread(input);
                     break;
                 case "exit":
                     isExit = true;
                     break;
                 default:
-                    System.out.println("Wrong command!");
+                    view.display("Wrong command!");
             }
         }
+        System.exit(0);
+    }
 
+    private void stopTread(String[] input) {
+        int index = 1;
+        if(input.length > 1 && container.containsKey(input[index])) {
+            container.stop(input[index]);
+        } else {
+            view.display("I didn't find this thread");
+        }
+    }
+
+    private void check(String[] input) {
+        int nameIndex = 1;
+        if(container.isEmpty()) {
+            view.display("No threads here");
+        } else if(input.length == 1) {
+            view.display(container);
+        } else if(container.containsKey(input[nameIndex])) {
+            view.display(container.get(input[nameIndex]));
+        } else {
+            view.display("No threads here!");
+        }
+    }
+
+    private void createThread(String[] input) {
+        int indexThreadName = 1;
+        container.addThread(input[indexThreadName]);
     }
 
 
