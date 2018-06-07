ecceac56e093317f5a054f225bb804a2b7501bf8
Adrian Bala <aadrianbala@gmail.com>
2018-06-07 13:50:16 +0200

    Add temporary solution for stopping threads

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -4,31 +4,41 @@ public class Timer implements Runnable {
 
     private int second;
     private String name;
+    private boolean isAlive;
     private static int timerCount = 0;
     private final int threadId = ++timerCount;
 
     public Timer(String name) {
         this.name = name;
-        second = 0;
+        this.second = 0;
+        this.isAlive = true;
     }
 
     public Timer(String name, int second) {
         this.name = name;
         this.second = second;
+        this.isAlive = true;
     }
 
     public void run() {
         try {
-            while(true) {
+            while(isAlive) {
                 TimeUnit.SECONDS.sleep(1);
                 second++;
-                if(Thread.interrupted()) {
-                    throw new InterruptedException();
-                }
+
             }
         } catch(InterruptedException e) {
-
+            return;
         }
+    }
+
+    public boolean stop() {
+        isAlive = false;
+        return isAlive;
+    }
+
+    public int getSecond() {
+        return second;
     }
 
     @Override
