09be19b26c6fcb4830935396236d1abb41104c59
Adrian Bala <aadrianbala@gmail.com>
2018-06-07 08:37:23 +0200

    Just in case - laptop problems

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-    }
+        Controller.start();
+//        Map<String, Timer> threads = new HashMap<String, Timer>();
+//        Timer first = new Timer("First");
+//        Thread t1 = new Thread(first);
+//        t1.start();
+//        Timer sec  = new Timer("Second");
+//        Thread t2 = new Thread(sec);
+//        Thread.sleep(1000);
+//        t2.start();
+//        Timer tree = new Timer("Third");
+//        Thread t3 = new Thread(tree);
+//        Thread.sleep(2000);
+//        t3.start();
+//        threads.put("First", first);
+//        threads.put("Second", sec);
+//        threads.put("Third", tree);
+//        for(String key : threads.keySet()) {
+//            System.out.println(threads.get(key));
+//        }
+//        t1.interrupt();
+//        Thread.sleep(4000);
+//        for(String key : threads.keySet()) {
+//            System.out.println(threads.get(key));
+//        }
+//        System.exit(0);
+        }
 }
