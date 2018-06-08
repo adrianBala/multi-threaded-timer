5a623f2ca0f607df6582a5e009ebae996f85eeed
Adrian Bala <aadrianbala@gmail.com>
2018-06-08 09:31:08 +0200

    Delete unnecessary import statements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
